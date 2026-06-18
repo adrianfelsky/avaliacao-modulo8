0e3cbef2a00b5a3f5d894c1dde051b4ebc33e64c
mrjoselucas <joselucasmr93@gmail.com>
2026-06-18 20:59:39 -0300

    Feature/repositorios (#10)
    * Implementando o repositório dos livros
    
    * Implementando o repositório dos livros
    
    * Add using directives for Interfaces and Modelos
    
    Adição das pastas sendo utilizadas.
    
    ---------
    
    Co-authored-by: Adrian Gazzani <142279062+adrianfelsky@users.noreply.github.com>

---
+++ Repositorios/Repositorios.cs
@@ -0,0 +1,37 @@
+﻿using BibliotecaApp.Excecoes;
+using BibliotecaApp.Interfaces;
+using BibliotecaApp.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApp.Repositorios
+{
+    public class RepositorioLivro : IRepositorioLivro
+    {
+        private List<Livros> _livros = new List<Livro>();
+
+        public void Adicionar(Livro livro)
+        {
+            _livros.Add(livro);
+        }
+        public List<Livros> ListarTodos()
+        {
+            return _livros.OrderBy(l => l.Titulo).ToList();
+        }
+        public Livro BuscarPorId(int id)
+        {
+            Livro livro = _livros.FirstOrDefault(l => l.Id == id);
+            return livro ?? throw new LivroNaoEncontradoException(id);
+        }              
+        public List<Livro> BuscarPorAutor(string autor)
+        {
+            return _livros
+                .Where(l => l.Autor.ToLower().Contains(autor.ToLower()))
+                .ToList();
+        }
+    }
+}
+
