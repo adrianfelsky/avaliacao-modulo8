f7e245e8728d7b6349bafbe609b41c3bcea7d9e9
Everson Oliveira <everson.dev11@hotmail.com>
2026-06-18 20:38:35 -0300

    Interface (#7)
    * Correção da posição da interface.
    
    * Delete Excecoes/LivroNaoEncontradoException.cs
    
    ---------
    
    Co-authored-by: Adrian Gazzani <142279062+adrianfelsky@users.noreply.github.com>

---
+++ interfaces/IRepositorioLivro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApp.interfaces
+{
+    public interface IRepositorioLivro
+    {
+        void Adicionar(Livros livro);
+
+        Livros BuscarPorId(int id);
+
+        IEnumerable<Livros> ListarTodos();
+
+        IEnumerable<Livros> BuscarPorAutor(string autor);
+
+        Task<IEnumerable<Livros>> ListarDisponiveisAsync();
+
+        Task<string> BuscarDetalhesApiAsync(string titulo);
+    }
+}
