97c379c028d21fd557c7d7d9fec9465748df5e51
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-06-18 20:54:23 -0300

    Feat: menu do sistema (#8)
    * feat: menu basico do sistema - R10
    
    * fix: colocado descrição nas propriedades e tirado o privado do bool

--- Program.cs
+++ Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿void PausaParaLer()
+{
+    Console.Write("\nDigite ENTER para continuar...");
+    Console.ReadLine();
+}
+
+bool sair = false;
+while (!sair)
+{
+    Console.Clear();
+    Console.WriteLine("1 - Listar Todos ");
+    Console.WriteLine("2 - Buscar por Id");
+    Console.WriteLine("3 - Buscar por Autor");
+    Console.WriteLine("4 - Listar Disponíveis (async) ");
+    Console.WriteLine("5 - Busca na API");
+    Console.WriteLine("6 - Salvar Acervo - R9");
+    Console.WriteLine("0 - sair");
+    Console.Write("Escolha uma opção do Menu: ");
+    string entrada = Console.ReadLine();
+
+    Console.Clear();
+    switch (entrada)
+    {
+        case "1":
+            //IMPLEMENTAR 
+            PausaParaLer();
+            break;
+        case "2":
+            //IMPLEMENTAR 
+            PausaParaLer();
+            break;
+        case "3":
+            //IMPLEMENTAR 
+            PausaParaLer();
+            break;
+        case "4":
+            //IMPLEMENTAR 
+            PausaParaLer();
+            break;
+        case "5":
+            //IMPLEMENTAR 
+            PausaParaLer();
+            break;
+        case "6":
+            //IMPLEMENTAR 
+            PausaParaLer();
+            break;
+        case "0":
+            sair = true;
+            break;
+        default:
+            Console.WriteLine("Digite uma opção válida!");
+            break;
+    }
+}
