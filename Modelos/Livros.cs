f4802dfb84eedb16372e2ac14f5f37b2229f9273
GabrielHenriqueCe <ga.biel.hce@gmail.com>
2026-06-18 20:44:06 -0300

    fix: colocado descrição nas propriedades e tirado o privado do bool

--- Modelos/Livros.cs
+++ Modelos/Livros.cs
@@ -8,30 +8,49 @@ public class Livros
     private int _ano;
     private bool _disponivel;
 
+    /// <summary>
+    /// Identificador único
+    /// </summary>
     public int ID
     {
         get { return _id; }
         private set { _id = value; }
     }
+
+    /// <summary>
+    /// Título do Livro
+    /// </summary>
     public string Titulo
     {
         get { return _titulo; }
         private set { _titulo = value; }
     }
+
+    /// <summary>
+    /// Nome do Autor
+    /// </summary>
     public string Autor
     {
         get { return _autor; }
         private set { _autor = value; }
     }
+
+    /// <summary>
+    /// Ano de publicação
+    /// </summary>
     public int Ano
     {
         get { return _ano; }
         private set { _ano = value; }
     }
+
+    /// <summary>
+    /// true disponível para empréstimo
+    /// </summary>
     public bool Disponivel
     {
         get { return _disponivel; }
-        private set { _disponivel = value; }
+        set { _disponivel = value; }
     }
 
     public Livros (int id, string titulo,  string autor, int ano, bool disponivel)
