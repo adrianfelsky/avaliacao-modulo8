ddc783dbd66ce645e2898ab0ac49b6e1d6fca73a
Adrian Gazzani <adriangazzani@gmail.com>
2026-06-18 19:41:10 -0300

    Adição de exceção para busca por livro não encontrado.

---
+++ Excecoes/LivroNaoEncontradoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApp.Excecoes
+{
+    public class ProdutoNaoEncontradoException : Exception
+    {
+        public ProdutoNaoEncontradoException(int id, string titulo)
+            : base($"Livrp #{id} - \"{titulo}\" não encontrado.") { }
+
+    }
+}
